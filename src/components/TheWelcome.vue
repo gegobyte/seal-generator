618eba669974471520bcc5ca45715da625db2aaa
Chinmay Sarupria <chinmaysarupria@gmail.com>
2024-02-20 08:53:56 +0530

    ready for deployment

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,117 @@
+<script setup>
+import html2canvas from "html2canvas";
+import { PDFDocument, StandardFonts } from "pdf-lib";
+import { ref } from "vue";
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+
+const firstLine = ref("");
+const secondLine = ref("");
+
+const sealPlacements = [
+  { page: 4, x: 400, y: 205 },
+  { page: 5, x: 75, y: 75 },
+  { page: 8, x: 450, y: 545 },
+  { page: 9, x: 465, y: 80 },
+  { page: 10, x: 50, y: 75 },
+  { page: 12, x: 50, y: 75 },
+  { page: 13, x: 50, y: 320 },
+  { page: 13, x: 50, y: 75 },
+  { page: 14, x: 50, y: 580 },
+  { page: 14, x: 50, y: 75 },
+  { page: 17, x: 40, y: 68 },
+  { page: 18, x: 40, y: 300 },
+  { page: 18, x: 40, y: 68 },
+  { page: 19, x: 40, y: 420 },
+  { page: 19, x: 40, y: 130 },
+  { page: 21, x: 300, y: 500 },
+  { page: 21, x: 300, y: 400 },
+  { page: 21, x: 300, y: 310 },
+  { page: 21, x: 300, y: 250 },
+  { page: 21, x: 40, y: 120 },
+  { page: 23, x: 465, y: 760 },
+];
+
+const printSeal = (pdfDoc, font) => {
+  const fontSize = 12;
+  const lineSpacing = 40;
+
+  sealPlacements.forEach((placement) => {
+    const page = pdfDoc.getPages()[placement.page - 1];
+
+    page.drawText(firstLine.value, {
+      x: placement.x,
+      y: placement.y,
+      size: fontSize,
+    });
+
+    // Measure the width of the first line of text
+    const widthOfFirstLine = font.widthOfTextAtSize(firstLine.value, fontSize);
+
+    // Calculate the x-coordinate for the start of the second line of text
+    const xStartSecondLine =
+      placement.x +
+      widthOfFirstLine -
+      font.widthOfTextAtSize(secondLine.value, fontSize);
+
+    // Draw the second line of text aligned with the end of the first line
+    page.drawText(secondLine.value, {
+      x: xStartSecondLine,
+      y: placement.y - lineSpacing, // Adjust Y-coordinate for the second line
+      size: fontSize,
+    });
+  });
+};
+
+const generateSealImage = async () => {
+  const sealElement = document.getElementById("seal-container");
+  const canvas = await html2canvas(sealElement, { scale: 1 });
+
+  // const hufPdfUrl = "/huf_form.pdf";
+  const hufPdfUrl = 'https://zerodha-common.s3.ap-south-1.amazonaws.com/Downloads-and-resources/ZerodhaNonindividualTD.pdf';
+  const existingPdfBytes = await fetch(hufPdfUrl).then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  // Get the fifth page of the document (zero-based index)
+  const page = pdfDoc.getPages()[4];
+
+  printSeal(pdfDoc, font);
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+
+  // Create a blob and trigger download
+  const blob = new Blob([pdfBytes], { type: "application/pdf" });
+  const url = window.URL.createObjectURL(blob);
+
+  // Open the PDF in a new tab
+  window.open(url, "_blank");
+};
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Seal Text</template>
+    <template #content>
+      <input type="text" v-model="firstLine" placeholder="Seal Line #1" />
+      <input type="text" v-model="secondLine" placeholder="Seal Line #2" />
+    </template>
+    <template #seal-preview>
+      <p>{{ firstLine }}</p>
+      <br /><br />
+      <p>{{ secondLine }}</p>
+    </template>
+    <template #generate-button>
+      <button @click="generateSealImage">Generate</button>
+    </template>
+  </WelcomeItem>
+</template>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h2>
+        <slot name="heading"></slot>
+      </h2>
+      <div class="content-wrapper">
+        <slot name="content"></slot>
+      </div>
+      <div class="seal-preview-wrapper">
+        <div id="seal-container"><slot name="seal-preview"></slot></div>
+      </div>
+      <div class="generate-button-wrapper">
+        <slot name="generate-button"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+.details {
+  flex: 1;
+  margin-left: 1rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+
+  color: var(--color-text);
+}
+
+h2 {
+  margin-bottom: 15px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+.content-wrapper,
+.generate-button-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 10px; /* Adjust the space between items */
+}
+
+.generate-button-wrapper {
+  margin-top: 10px;
+}
+
+.generate-button-wrapper ::v-deep button {
+  font-size: 1.1em;
+  width: 30%;
+  padding: 10px 20px;
+  display: inline-block;
+  background-color: #283b49;
+  color: white;
+  border: 1px solid transparent; /* Adjusts the border */
+  box-shadow: none;
+}
+
+.content-wrapper ::v-deep input {
+  font-size: 1.1em;
+  padding: 10px 20px;
+  display: inline-block;
+  box-shadow: none;
+}
+
+.seal-preview-wrapper {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 1px dotted black;
+  padding: 10px;
+  display: inline-block;
+}
+
+.seal-preview-wrapper ::v-deep p {
+  text-align: right;
+  font-weight: bold;
+  line-height: normal;
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  i {
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+
+  .item:before {
+    content: " ";
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:after {
+    content: " ";
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:first-of-type:before {
+    display: none;
+  }
+
+  .item:last-of-type:after {
+    display: none;
+  }
+}
+</style>
