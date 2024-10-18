56601528e578f59237ae8ffc5f306d87f894e47b
Chinmay Sarupria <chinmaysarupria@gmail.com>
2024-10-18 17:20:42 +0530

    minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Huf from "../components/huf/Huf.vue";
 import Corporate from "../components/corporate/Corporate.vue";
 import Partnership from "../components/partnership/Partnership.vue";
 import Llp from "../components/llp/llp.vue";
-import SealPage from "../components/SealPage.vue";
 
 const routes = [
   {
@@ -25,11 +24,6 @@ const routes = [
     path: "/llp",
     name: "Llp",
     component: Llp,
-  },
-  {
-    path: "/seal",
-    name: "Seal",
-    component: SealPage,
   },
 ];
 
